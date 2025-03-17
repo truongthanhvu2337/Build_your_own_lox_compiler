12368ee783ee449eb8fc708b16f3bdde95ddf944
truongthanhvu2337 <truongthanhvu2337@gmail.com>
2025-03-17 18:11:21 +0700

    test

--- internal/token/token.go
+++ internal/token/token.go
@@ -1,21 +1,20 @@
 package token
 
-type TokenType int
+type TokenType string
 
 const (
-	LEFT_PAREN TokenType = iota
-	RIGHT_PAREN
-	LEFT_BRACE
-	RIGHT_BRACE
-	COMMA
-	DOT
-	MINUS
-	PLUS
-	SEMICOLON
-	SLASH
-	STAR
-
-	EOF
+	LEFT_PAREN  TokenType = "LEFT_PAREN"
+	RIGHT_PAREN TokenType = "RIGHT_PAREN"
+	LEFT_BRACE  TokenType = "LEFT_BRACE"
+	RIGHT_BRACE TokenType = "RIGHT_BRACE"
+	COMMA       TokenType = "COMMA"
+	DOT         TokenType = "DOT"
+	MINUS       TokenType = "MINUS"
+	PLUS        TokenType = "PLUS"
+	SEMICOLON   TokenType = "SEMICOLON"
+	SLASH       TokenType = "SLASH"
+	STAR        TokenType = "STAR"
+	EOF         TokenType = "EOF"
 )
 
 type Token struct {
